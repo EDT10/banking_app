8dcbb9419e060d51a68bbe96ec0a7e33390da969
Emmanuel Wright <emmanuelnwright@gmail.com>
2022-07-09 10:12:38 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+def atm_menu(name):
+    print("")
+    print("          === Automated Teller Machine ===          ")
+    print("User: " + name)
+    print("------------------------------------------")
+    print("| 1.    Balance     | 2.    Deposit      |")
+    print("------------------------------------------")
+    print("------------------------------------------")
+    print("| 3.    Withdraw    | 4.    Logout       |")
+    print("------------------------------------------")
